9b8619d99d903dfd07d29be82a02a06cc3bb4675
Ritesh DB <riteshdb123@gmail.com>
2022-08-06 14:51:30 +0530

    Second commit

--- PrintClass.java
+++ PrintClass.java
@@ -1,5 +1,6 @@
 public class PrintClass {
     public static void main(String[] args) {
         System.out.println("This is First Line of the project");
+        System.out.println("This is second line of the project");
     }
 }
